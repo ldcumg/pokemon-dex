0696f0ff7252059a2f5d36f6548c614ae4231dd6
ldcumg <ldcumg@naver.com>
2024-08-22 15:28:25 +0900

    코드 구조 수정
    useLocation state 객체화

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,9 +1,33 @@
 import { useContext } from "react";
 import { PokemonContext } from "../context/PokemonContext";
 import PokemonCard from "./PokemonCard";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const PokemonList = () => {
-  const { MOCK_DATA } = useContext(PokemonContext);
+  const {
+    MOCK_DATA,
+    selectedPokemon,
+    setSelectedPokemon,
+    selectedMark,
+    setSelectedMark,
+  } = useContext(PokemonContext);
+
+  const connectedScrollY = useLocation().state.current.scrollY ?? 0;
+  const connectedList =
+    useLocation().state.current.selectedPokemon ?? selectedPokemon;
+  const connectedSelectedMark =
+    useLocation().state.current.selectedMark ?? selectedMark;
+
+  useEffect(() => {
+    connectedScrollY &&
+      window.scrollTo({
+        top: connectedScrollY,
+      });
+    setSelectedPokemon(connectedList);
+    setSelectedMark(connectedSelectedMark);
+  }, []);
+
   return (
     <ul id="pokemon-list-container">
       {MOCK_DATA.map((pokemon) => {
